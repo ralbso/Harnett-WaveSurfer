834dfc36727cfe805b191b3f048c10047cd90b02
Raúl <rsotoalbors@gmail.com>
2024-03-11 16:57:24 -0400

    our NI PCIe can support 4 channels in diff mode

--- +ws/getNumberOfDifferentialAITerminalsFromDevice.m
+++ +ws/getNumberOfDifferentialAITerminalsFromDevice.m
@@ -2,6 +2,6 @@ function result = getNumberOfDifferentialAITerminalsFromDevice(deviceName)
     % The number of AI channels available, if you used them all in
     % *differential* mode, which we do.
     nSingleEnded = ws.getNumberOfSingleEndedAITerminalsFromDevice(deviceName) ;  % the number of channels available if you used them all in single-ended mode
-    result = round(nSingleEnded/2) ;  % the number of channels available if you use them all in differential mode, which we do
+    result = round(nSingleEnded) ;  % the number of channels available if you use them all in differential mode, which we do
 end
 
